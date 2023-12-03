ea4585620d7b6e57e369ca7873b71a8b2c0353de
rmarkowi1990 <rick@fastcasual.co>
2023-12-02 16:43:10 -0800

    created files with Redux Toolkit, React Native framework

---
+++ Leftovers/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from '/components/Home.js'
+
+export default function App() {
+  return (
+    <div>
+      <Home />
+    </div >
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Leftovers/components/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        < div >
+            <h3>Hello This Is My Website!</h3>
+        </div >
+
+    )
+}
