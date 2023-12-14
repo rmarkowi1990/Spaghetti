931f95550852abd3cc2ef34131f6698670ba8dad
rmarkowi1990 <rick@fastcasual.co>
2023-12-13 16:00:41 -0800

    merging cardAdjustment

--- Pages/Error404.jsx
+++ Pages/Error404.jsx
@@ -6,14 +6,11 @@ export default function Error404() {
 
     const navigate = useNavigate()
 
-
     return (
         <div className="error404">
             <h1>Oh No!</h1>
             <h2>We <i>spilled</i>  the Spaghetti.</h2>
             <button onClick={() => navigate('/')}>return</button>
-
-
         </div>
     )
 }
--- Pages/Feed.jsx
+++ Pages/Feed.jsx
@@ -6,22 +6,13 @@ import MealCard from '../Components/MealCard.jsx'
 export default function Feed() {
 
 
-
-    // useEffect(() => {
-
-
-
-
-
-    // }, [])
-
     return (
         <div className='background'>
             <div id="mainFeed">
-                <h1>leftovers near you</h1>
+                <h1>pounds of leftovers:</h1>
                 <div className='feedGrid'>
                     <MealCard name="my famous lasagna" chef="HaleyLo32" rating="4.9" days="1.5" price="$5.43" image="https://www.closetcooking.com/wp-content/uploads/2009/02/Lasagna-Messy-500.jpg" />
-                    <MealCard name="chicken nuggets" chef="BenCooks83" rating="3.5" days="2" price="$3.31" image="https://www.melaniecooks.com/wp-content/uploads/2013/01/chicken-nuggets-not-soggy.jpg" />
+                    <MealCard name="fun nuggets" chef="BenCooks83" rating="3.5" days="2" price="$3.31" image="https://www.melaniecooks.com/wp-content/uploads/2013/01/chicken-nuggets-not-soggy.jpg" />
                     <MealCard name="tuna calzone sandwich" chef="DeborahLovesTuna" rating="4.3" days="1" price="$4.31" image="https://i.redd.it/ip590b04e0151.jpg" />
                     <MealCard name="panda express leftovers" chef="DinnerDale" rating="2.8" days="1" price="$2.59" image="https://paladinipotpie.files.wordpress.com/2012/07/leftover-chinese-food.jpg" />
                     <MealCard name="slime dogs" chef="MrSlime" rating="1.3" days="4" price="$1.31" image="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2018_04/1312948/pb-hotdog-20180124-inline.jpg" />
