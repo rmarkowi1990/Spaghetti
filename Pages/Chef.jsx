bc5493570e9e1f4aee3bcdb19b1cb4aed72bf9dc
rmarkowi1990 <rick@fastcasual.co>
2023-12-14 21:57:24 -0800

    branching new layout for Chef

--- Pages/Chef.jsx
+++ Pages/Chef.jsx
@@ -45,89 +45,97 @@ export default function Chef() {
 
     return (
         <div id='chefTableContainer'>
+            <div id='container2'></div>
             <h1>{username}'s Chef Table</h1>
-            <div id='mealSubmission'>
 
 
-                <div id='imageSection'>
-                    <img></img>
-                    <input type='file' onChange={(event) => dispatch(addImage(event.target.files[0]))} />
-                </div>
-                <div className='formSection'>
-                    <h2>new leftovers:</h2>
+            <div id='outerMeal'>
+                <h1>New Chef Creation</h1>
+                <div id='mealSubmission'>
 
-                    <div className="chefField">
-                        <h3>Meal Title</h3>
-                        <input onInput={(event) => dispatch(enterMealTitle(event))} value={mealTitle}></input>
+
+                    <div id='imageSection'>
+                        <img></img>
+                        <input type='file' onChange={(event) => dispatch(addImage(event.target.files[0]))} />
                     </div>
+                    <div className='formSection'>
 
-                    {/* <div className="entryField">
+                        <div className="chefField">
+                            <h3>Meal Title</h3>
+                            <input onInput={(event) => dispatch(enterMealTitle(event))} value={mealTitle}></input>
+                        </div>
+
+                        {/* <div className="entryField">
                         <h3>Date Cooked</h3>
                         <input type="date" ></input>
                     </div> */}
 
-                    <div className="chefField">
-                        <h3>Expiration Date</h3>
-                        <input type="date" onChange={(date) => dispatch(enterExpiration(date))} value={expiration}></input>
-                    </div>
-
-
-
-                    <div className="chefField">
-                        <h3>Price Ceiling</h3>
-                        <input onInput={(event) => dispatch(enterPrice(event))} value={price} ></input>
-                    </div>
-
-                    <div className="ingredients">
-                        <h3>Ingredients</h3>
-                        <div className="ingredientsWrapper">
-                            <div className="ingredientsList">
-
-                                <span> <input type="checkbox" id="dairy" name="dairy" onClick={() => dispatch(toggleDairy())} checked={dairy} />
-                                    <label for="dairy">Dairy</label></span>
-
-                                <span><input type="checkbox" id="eggs" name="eggs" onClick={() => dispatch(toggleEggs())} checked={eggs} />
-                                    <label for="eggs">Eggs</label></span>
-
-                                <span><input type="checkbox" id="fish" name="fish" onClick={() => dispatch(toggleFish())} checked={fish} />
-                                    <label for="fish">Fish</label></span>
-
-                                <span><input type="checkbox" id="crustaceans" name="crustaceans" onClick={() => dispatch(toggleCrustaceans())} checked={crustaceans} />
-                                    <label for="crustaceans">Crustaceans</label></span>
-
-                                <span><input type="checkbox" id="treeNuts" name="treeNuts" onClick={() => dispatch(toggleTreeNuts())} checked={treeNuts} />
-                                    <label for="treeNuts">Tree Nuts</label></span>
-                            </div>
-                            <div className="ingredientsList">
-
-                                <span><input type="checkbox" id="peanuts" name="peanuts" onClick={() => dispatch(togglePeanuts())} checked={peanuts} />
-                                    <label for="peanuts">Peanuts</label></span>
-
-                                <span><input type="checkbox" id="wheat" name="wheat" onClick={() => dispatch(toggleWheat())} checked={wheat} />
-                                    <label for="wheat">Wheat</label></span>
-
-                                <span><input type="checkbox" id="soybeans" name="soybean" onClick={() => dispatch(toggleSoybeans())} checked={soybeans} />
-                                    <label for="soybeans">Soybeans</label></span>
-
-                                <span><input type="checkbox" id="sesame" name="sesame" onClick={() => dispatch(toggleSesame())} checked={sesame} />
-                                    <label for="sesame">Sesame</label></span>
-
-                                <span><input type="checkbox" id="meat" name="meat" onClick={() => dispatch(toggleMeat())} checked={meat} />
-                                    <label for="meat">Meat</label></span>
-                            </div>
+                        <div className="chefField">
+                            <h3>Expiration Date</h3>
+                            <input type="date" onChange={(date) => dispatch(enterExpiration(date))} value={expiration}></input>
                         </div>
 
 
 
+                        <div className="chefField">
+                            <h3>Price Ceiling</h3>
+                            <input onInput={(event) => dispatch(enterPrice(event))} value={price} ></input>
+                        </div>
+
+                        <div className="ingredients">
+                            <h3>Ingredients</h3>
+                            <div className="ingredientsWrapper">
+                                <div className="ingredientsList">
+
+                                    <span> <input type="checkbox" id="dairy" name="dairy" onClick={() => dispatch(toggleDairy())} checked={dairy} />
+                                        <label for="dairy">Dairy</label></span>
+
+                                    <span><input type="checkbox" id="eggs" name="eggs" onClick={() => dispatch(toggleEggs())} checked={eggs} />
+                                        <label for="eggs">Eggs</label></span>
+
+                                    <span><input type="checkbox" id="fish" name="fish" onClick={() => dispatch(toggleFish())} checked={fish} />
+                                        <label for="fish">Fish</label></span>
+
+                                    <span><input type="checkbox" id="crustaceans" name="crustaceans" onClick={() => dispatch(toggleCrustaceans())} checked={crustaceans} />
+                                        <label for="crustaceans">Crustaceans</label></span>
+
+                                    <span><input type="checkbox" id="treeNuts" name="treeNuts" onClick={() => dispatch(toggleTreeNuts())} checked={treeNuts} />
+                                        <label for="treeNuts">Tree Nuts</label></span>
+                                </div>
+                                <div className="ingredientsList">
+
+                                    <span><input type="checkbox" id="peanuts" name="peanuts" onClick={() => dispatch(togglePeanuts())} checked={peanuts} />
+                                        <label for="peanuts">Peanuts</label></span>
+
+                                    <span><input type="checkbox" id="wheat" name="wheat" onClick={() => dispatch(toggleWheat())} checked={wheat} />
+                                        <label for="wheat">Wheat</label></span>
+
+                                    <span><input type="checkbox" id="soybeans" name="soybean" onClick={() => dispatch(toggleSoybeans())} checked={soybeans} />
+                                        <label for="soybeans">Soybeans</label></span>
+
+                                    <span><input type="checkbox" id="sesame" name="sesame" onClick={() => dispatch(toggleSesame())} checked={sesame} />
+                                        <label for="sesame">Sesame</label></span>
+
+                                    <span><input type="checkbox" id="meat" name="meat" onClick={() => dispatch(toggleMeat())} checked={meat} />
+                                        <label for="meat">Meat</label></span>
+                                </div>
+                            </div>
+
+
+
+
+                        </div>
+                        <h3>Description</h3>
+                        <textarea id="description" onInput={(event) => dispatch(enterDescription(event))} value={description}></textarea>
+
 
                     </div>
-                    <h3>Description</h3>
-                    <textarea id="description" onInput={(event) => dispatch(enterDescription(event))} value={description}></textarea>
-
-                    <button onClick={submit}>Share</button>
-
 
                 </div>
+
+                <button onClick={submit}>Share</button>
+
+
             </div>
 
         </div >
