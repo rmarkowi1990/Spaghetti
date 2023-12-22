6961b9827ec7fde9ac4633474efc73d8d824736f
rmarkowi1990 <rick@fastcasual.co>
2023-12-22 17:24:15 -0500

    ingredient script function working

--- Pages/Preview.jsx
+++ Pages/Preview.jsx
@@ -9,40 +9,57 @@ import { storePreview } from '../Redux/mealsSlice';
 
 export default function Preview(props) {
 
-    const dispatch = useDispatch()
-    const location = useLocation()
+    const dispatch = useDispatch();
+    const location = useLocation();
 
-    // const { mealtitle, chef, rating, url, price, expiration, description, dairy, eggs, fish, crustaceans, treenuts, peanuts, wheat, soybeans, sesame, meat, portions } = useSelector(state => state.meals.preview)
-    const preview = useSelector((state) => state.meals.preview)
-    const passedID = location.state
+    const meals = useSelector((state) => state.meals.meals);
 
 
 
-    useEffect(() => {
-        fetch(`http://localhost:3000/meals/meal/${passedID}`)
-            .then(res => res.json())
-            .then(data => {
-                dispatch(storePreview(data[0]));
 
-            })
+    const passedID = location.state;
+
+    const preview = meals.filter((meal) => meal.meal_id === passedID)[0];
+    const { dairy, eggs, fish, crustaceans, treenuts, peanuts, wheat, soybeans, sesame, meat } = preview;
 
 
-    }, [])
+    // console.log('preview: ', preview);
+
+    function ingredientScript() {
+        let output = ''
+        const list = { dairy, eggs, fish, crustaceans, treenuts, peanuts, wheat, soybeans, sesame, meat }
+        let keys = Object.keys(list);
+        keys = keys.sort();
+
+        for (let i in keys) {
+            if (list[keys[i]]) {
+                let ingredient = keys[i];
+                let firstLetter = ingredient[0];
+                ingredient = ingredient.slice(1);
+                output = output + firstLetter.toUpperCase() + ingredient + ', '
+            }
+        }
+
+        output = output.slice(0, -2)
+        return output;
+    }
+
+
 
 
 
     return (
         <div className='previewContainer'>
 
-            <h1 id='previewTitle'>{preview && preview.mealtitle}</h1>
-            <h2 id='previewChef'>a creation by <span id='previewUsername'>{preview && preview.chef}</span></h2>
+            <h1 id='previewTitle'>{preview.mealtitle}</h1>
+            <h2 id='previewChef'>a creation by <span id='previewUsername'>{preview.chef}</span></h2>
 
             <div className='previewGrid'>
 
-                <img id='previewImage' src={preview && preview.url}></img>
+                <img id='previewImage' src={preview.url}></img>
                 <div id='previewRight'>
-
-                    <p>{preview && preview.description}</p>
+                    <h3>Contains:</h3>
+                    <p>{ingredientScript()}</p>
                 </div>
             </div>
 
